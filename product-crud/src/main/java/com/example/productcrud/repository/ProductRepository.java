d99b062e389638c91504112b65fad4aeb62465a7
chris315-psyga <chris.maruli7@gmail.com>
2026-04-08 21:11:26 +0700

    handson-8

---
+++ product-crud/src/main/java/com/example/productcrud/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.example.productcrud.repository;
+import com.example.productcrud.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+}
---
+++ product-crud/src/main/java/com/example/productcrud/service/ProductService.java
@@ -0,0 +1,40 @@
+package com.example.productcrud.service;
+
+import com.example.productcrud.model.Category;
+import com.example.productcrud.model.Product;
+import com.example.productcrud.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    private final List<Product> products = new ArrayList<>();
+    private Long nextId = 7L;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+}
